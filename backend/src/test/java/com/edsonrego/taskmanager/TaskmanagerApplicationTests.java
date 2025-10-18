33bd9195d0bd7fa4ad25130407d977e12d2bf52d
EdsonRego <edsonxrego@gmail.com>
2025-10-18 09:24:52 -0300

    Initial commit - backend Spring Boot structure

---
+++ backend/src/test/java/com/edsonrego/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edsonrego.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
